0454b2633d5943490ddb14a4c5bfcda0bbf5ce3d
Tushar H <tusharjain2201@gmail.com>
2024-06-07 11:51:26 +0530

    Basic UI of the tab

---
+++ src/assets/asset.jsx
@@ -0,0 +1,15 @@
+import base from './base.png';
+import pointer from './pointer.png';
+import doubleArrow from './double-arrow.png';
+import divider from './divider.png';
+import slant from './slant.png';
+import arrow from './arrow.png';
+
+export {
+    base,
+    pointer,
+    doubleArrow,
+    divider,
+    slant,
+    arrow
+}
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,10 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Slider.css';
+import { arrow, base, divider, doubleArrow, pointer, slant } from '../../assets/asset';
 
 const Slider = () => {
-  return (
-    <div className='slider'>
+  const data = ['TENANT 1', 'TENANT 2', 'TENANT 3', 'TENANT 4', 'TENANT 5', 'TENANT 6', 'TENANT 7', 'TENANT 8', 'TENANT 9', 'TENANT 10'];
 
+  const [currentTab, setCurrentTab] = useState(0);
+
+  const scrollLeft = () => {
+    if (currentTab !== 0) {
+      setCurrentTab(currentTab - 1);
+    }
+  }
+
+  const scrollRight = () => {
+    if (currentTab !== data.length - 1) {
+      setCurrentTab(currentTab + 1);
+    }
+  }
+
+  useEffect(() => {
+
+    const tab = document.querySelector(`.ss-item:nth-child(${currentTab + 1})`);
+    if (tab) {
+      tab.scrollIntoView({ behavior: 'smooth' });
+    }
+
+  }, [currentTab]);
+
+  return (
+    <div className='ss-slider'>
+      <div className='ss-base'>
+        <img src={base} alt='base' />
+      </div>
+      <div className='ss-pointer'>
+        <img src={pointer} alt='pointer' />
+      </div>
+      <div className='ss-tabs'>
+        {data.map((item, index) => (
+          <div key={index} className={`ss-item ${currentTab === index ? 'active' : ''}`}>
+            {item}
+            <div className='ss-divider'>
+              <img src={divider} alt='divider' />
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className='ss-double-arrow left' onClick={scrollLeft}>
+        <div>
+          <img src={arrow} alt='arrow' />
+        </div>
+        <div>
+          <img src={arrow} alt='arrow' />
+        </div>
+      </div>
+      <div className='ss-double-arrow right' onClick={scrollRight}>
+        <div>
+          <img src={arrow} alt='arrow' />
+        </div>
+        <div>
+          <img src={arrow} alt='arrow' />
+        </div>
+      </div>
+      <div className='ss-slant left'>
+        <img src={slant} alt='slant' />
+      </div>
+      <div className='ss-slant right'>
+        <img src={slant} alt='slant' />
+      </div>
     </div>
   )
 }
